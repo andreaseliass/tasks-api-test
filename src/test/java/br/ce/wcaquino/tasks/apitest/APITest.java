a12e9800928feb394d2618759b41982aff005555
andreaselias <andreaase@hotmail.com>
2023-11-25 00:03:41 +0000

    teste de remocao

--- src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -1,8 +1,13 @@
 package br.ce.wcaquino.tasks.apitest;
 
+import java.net.MalformedURLException;
+
 import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -48,6 +53,29 @@ public class APITest {
 			.body("message", CoreMatchers.is("Due date must not be in past"))
 			;
 	}
+	
+	@Test
+	public void deveRemoverTarefaComSucesso() {
+		//inserir tarefa
+		Integer id = RestAssured.given()
+			.body("{ \"task\": \"Tarefa Teste\", \"dueDate\": \"2030-12-11\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201)
+			.extract().path("id")
+		;
+		System.out.println(id);
+		//remover
+		RestAssured.given()
+		.when()
+			.delete("/todo/"+id)
+		.then()
+			.statusCode(204)
+
+		;
+		}
 
 }
 
